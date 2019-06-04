00c7a6be9b28fd97797dc62d0ecd67c218990c62
cyntran <cyntran@uw.edu>
2019-06-04 13:48:14 -0700

    added route for profile

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import MainContainer from "./components/container/MainContainer.jsx"
 import MarketPlace from "./components/presentational/MarketPlace.jsx"
+import Profile from "./components/presentational/Profile.jsx"
 
 const routing = (
   <Router>
     <div>
     <Route exact={true} path="/" component={ MainContainer } />
     <Route path="/forsale" component={ MarketPlace } />
+    <Route path="/profile/:username" component={ Profile } />
     </div>
   </Router>
 )
