e30126a30e890d141e152aac48e39072e64491d1
cyntran <cyntran@uw.edu>
2019-06-06 00:37:41 -0700

    added new route for pet profiles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import MainContainer from "./components/container/MainContainer.jsx"
 import MarketPlace from "./components/presentational/MarketPlace.jsx"
 import Profile from "./components/presentational/Profile.jsx"
+import PetProfile from "./components/presentational/PetProfile.jsx"
 
 const routing = (
   <Router>
@@ -11,6 +12,7 @@ const routing = (
     <Route exact={true} path="/" component={ MainContainer } />
     <Route path="/forsale" component={ MarketPlace } />
     <Route path="/profile/:username" component={ Profile } />
+    <Route path="/pet/:username/:petname" component={ PetProfile } />
     </div>
   </Router>
 )
