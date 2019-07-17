040c5eee53965a4057503b5dee500dd94f0661d8
cyntran <cyntran@uw.edu>
2019-07-17 04:31:58 -0700

    server/purchase.js

---
+++ server/purchase.js
@@ -0,0 +1,180 @@
+let { getUserById } = require('../server/database.js')
+let { getRandomInt, isPetUnicode, isEmpty } = require('./dbScripts.js')
+
+
+// db => database
+// id => user id
+// buyData => { name, bio } for pet, item object for item
+// item => item object
+async function purchase (db, id, buyData, item) {
+  let userInfo = {}
+  try {
+    userInfo = await getUserById(db, id)
+    let updatedUserInfo = deepCopyUser(userInfo)
+    let newQuan = updateExistingUserItemQuan(item, updatedUserInfo)
+    setUserItemProperties(updatedUserInfo, item, buyData, newQuan)
+    updateUserBalance(updatedUserInfo, item.price)
+    if (notEnoughFunds(updatedUserInfo)) return false
+
+    await updatePurchaseItemQuantity(db, item)
+    let itemName = (buyData && buyData.name) ? buyData.name : buyData.unicode
+    await savePurchaseToUser(db, item,  itemName, updatedUserInfo)
+    await paySeller(db, buyData, id, 5)
+    await saveItemToMarket(db, item)
+    return updatedUserInfo
+  } catch (err) {
+    throw err
+  }
+}
+
+function updateUserBalance (userInfo, price) {
+   userInfo.balance -= price
+}
+
+function setUserItemProperties (updatedUserInfo, item, buyData, newQuan) {
+  let type = item.isAnimal ? 'pets' : 'items'
+  let nameOrUni = (type === 'pets') ? buyData.name : item.unicode
+  if (type === 'pets') {
+    updatedUserInfo[type][nameOrUni] =
+      {
+        "price": item.price,
+        "path": item.path,
+        "unicode": item.unicode,
+        "isAnimal": true,
+        "petData": {
+          "hunger": item.petData.hunger,
+          "generation": item.petData.generation,
+          "happiness": item.petData.happiness,
+          "intelligence": item.petData.intelligence,
+          "charisma": item.petData.charisma,
+          "health": item.petData.health,
+          "attractiveness": item.petData.attractiveness,
+          "age": item.petData.age,
+          "prevOwner": item.petData.prevOwner
+        },
+        name: buyData.name,
+        bio: buyData.bio
+      }
+  } else {
+    updatedUserInfo[type][nameOrUni] = {
+      "path": item.path,
+      "unicode": item.unicode,
+      "price": item.price,
+      "isAnimal": item.isAnimal,
+      "quantity": newQuan,
+      "category": item.category,
+      "product": item.product,
+      "isCrafted": item.isCrafted,
+      "seller": null
+    }
+  }
+}
+
+function notEnoughFunds (updatedUserInfo) {
+  return updatedUserInfo.balance < 0 ? true : false
+}
+
+async function updatePurchaseItemQuantity (db, item) {
+  // decrement quantity for market item
+  let key = (!item.name) ? item.unicode : item.name
+  if (item.quantity != null && item.quantity > 1) {
+    --item.quantity
+  } else {
+    await db.del(`emoji/forsale/${key}`)
+  }
+}
+
+function deepCopyUser (userInfo) {
+  let copyPets = {}
+  let copyItems = {}
+  let petKeys = Object.keys(userInfo.pets)
+  let itemKeys = Object.keys(userInfo.items)
+  for (let i in petKeys) {
+    copyPets[petKeys[i]] = {}
+    copyPets[petKeys[i]] = userInfo.pets[petKeys[i]]
+  }
+  for (let i in itemKeys) {
+    copyItems[itemKeys[i]] = {}
+    copyItems[itemKeys[i]] = userInfo.items[itemKeys[i]]
+  }
+  return {
+    id: userInfo.id,
+    username: userInfo.username,
+    email: userInfo.email,
+    hash: userInfo.hash,
+    balance: userInfo.balance,
+    pets: copyPets,
+    items: copyItems
+  }
+}
+
+function updateExistingUserItemQuan (item, updatedUserInfo) {
+  if (!item.isAnimal) {
+    let userHasItem = updatedUserInfo.items[item.unicode] != null
+    if (userHasItem) {
+      return ++updatedUserInfo.items[item.unicode].quantity
+    } else {
+      return 1
+    }
+  }
+}
+
+async function savePurchaseToUser(db, item, petName, updatedUserInfo) {
+  if (item.isAnimal) {
+    updatedUserInfo.pets[petName].petData.prevOwner = updatedUserInfo.id
+  }
+  await db.put(`user/${updatedUserInfo.id}`, updatedUserInfo)
+}
+
+async function saveItemToMarket (db, item) {
+  let type = item.isAnimal ? 'pets' : 'items'
+  if (type === 'pets' && !item.petData.prevOwner) {
+    item.petData = getNewPetStats(item.petData)
+  } else {
+    if (type === 'pets' && item.petData.prevOwner) {
+      await db.put(`emoji/forsale/${item.name}`, item)
+      return
+    }
+    if (item != null) {
+      await db.put(`emoji/forsale/${item.unicode}`, item)
+    }
+  }
+}
+
+
+// sellers only earn from selling pets right now
+// items can't be sold yet
+async function paySeller (db, buyData, id, amount) {
+  if (!buyData.isAnimal || !buyData.petData.prevOwner)  return
+  try {
+    let user = await db.get(`user/${id}`)
+    let sellerUpdated = Object.assign({}, user, {
+      balance: user.balance + amount
+    })
+    await db.put(`user/${buyData.petData.prevOwner}`, sellerUpdated)
+  } catch (err) {
+    throw err
+  }
+}
+
+function getNewPetStats (petData) {
+  return Object.assign({}, petData, {
+    intelligence: getRandomInt(),
+    charisma: getRandomInt(),
+    attractiveness: getRandomInt()
+  })
+}
+
+module.exports = {
+  purchase,
+  deepCopyUser,
+  getNewPetStats,
+  paySeller,
+  saveItemToMarket,
+  savePurchaseToUser,
+  updatePurchaseItemQuantity,
+  updateExistingUserItemQuan,
+  notEnoughFunds,
+  setUserItemProperties,
+  updateUserBalance
+}
