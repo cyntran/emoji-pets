211c1a7ee54f8a9df181313fd5d869d87869218e
cyntran <cyntran@uw.edu>
2019-07-17 04:37:12 -0700

    items that are not animals have their own container

---
+++ src/components/presentational/UserItem.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import ItemModal from "../presentational/ItemModal.jsx"
+
+class UserItem extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      isHidden: true,
+      item: this.props.item,
+      pets: this.props.pets
+    }
+  }
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.handleScroll)
+    let scrollPs = localStorage.getItem('scrollY')
+    if (scrollPs) {
+      setTimeout(() => window.scrollTo(0, scrollPs), 200)
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll () {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    window.localStorage.setItem('scrollY', winScroll)
+  }
+
+  checkHidden = (data) => {
+    console.log(data)
+    this.setState({ isHidden: data})
+  }
+
+  toggleHidden () {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
+
+  render () {
+    return (
+      <div className='items not-animal'>
+        { !this.state.isHidden && <ItemModal handleModal={this.checkHidden} item={this.state.item} pets={this.state.pets}/> }
+        <img src={this.state.item.path} id='item' />
+        <p id='item-category'>Category: {this.state.item.category}</p>
+        <button className='open-item-btn' onClick={this.toggleHidden.bind(this)}>options</button>
+      </div>
+    )
+  }
+}
+
+export default UserItem
