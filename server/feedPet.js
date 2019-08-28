01b485191fa48c9f35b2d86f2f423d136934c681
cyntran <cyntran@uw.edu>
2019-08-27 17:24:38 -0700

    helper functions to simplify code and readability

--- server/feedPet.js
+++ server/feedPet.js
@@ -4,13 +4,13 @@ let { getUserById } = require('./database.js')
 // PARAMS: food - food unicode, pet - pet object, user - user object
 async function feedPet (db, food, pet, user) {
   let foodItem = user.items[food]
+  
   if (foodItem == null) return user
 
   pet.petData.feeding = pet.petData.feeding || createFeedTimeObj()
 
   if (pet.petData.feeding.canFeed) {
     let feed = updateFeedTime(pet.petData.feeding)
-    console.log('feedTime after', feed)
     user = updatePetStats(foodItem, pet, user)
     try {
       await db.put(`user/${user.id}`, user)
@@ -25,14 +25,13 @@ async function feedPet (db, food, pet, user) {
   }
 }
 
-// subtract hunger by 5
-// increase health by 5
+
 function updatePetStats (foodItem, pet, user) {
   let score = (!foodItem.isCrafted) ? 2 : 5
   if (pet.petData.health >= 20) {
-    pet.petData.hunger = (pet.petData.hunger - 10 < 0) ? 0 : pet.petData.hunger - 10
-    pet.petData.health = (pet.petData.health + 10 > 100) ? 100 : pet.petData.health + 10
-    pet.petData.happiness = (pet.petData.happiness + score > 100) ? 100 : pet.petData.happiness + score
+    pet.petData.hunger = decrementHunger(pet.petData.hunger)
+    pet.petData.health = incrementHealth(pet.petData.health)
+    pet.petData.happiness = incrementHappiness(pet.petData.happiness, score)
     if ((foodItem.quantity - 1) <= 0) {
       delete user.items[foodItem.unicode]
     } else {
@@ -43,25 +42,46 @@ function updatePetStats (foodItem, pet, user) {
   return user
 }
 
+const HUNGER_DECR = 10
+const HEALTH_INCR = 10
+
+function decrementHunger (hunger) {
+  return (hunger < HUNGER_DECR) ? 0 : hunger - HUNGER_DECR
+}
+
+function incrementHealth (health) {
+  return (HEALTH_INCR + health > 100) ? 100 : health + HEALTH_INCR
+}
+
+function incrementHappiness (happiness, score) {
+  return (happiness + score > 100) ? 100 : happiness + score
+}
+
+function firstFeedWasWithin24Hours (hours, feedTime) {
+  return (hours - feedTime.first < 24 && feedTime.number < 3)
+}
+
+function firstFeedWas24HoursAgo (hours, feedTime) {
+  return (hours - feedTime.first >= 24)
+}
+
 function updateFeedTime (feedTime) {
   if (isEmpty(feedTime)) {
     feedTime = createFeedTimeObj()
   }
-  console.log('feedTime before', feedTime)
+  feedTime.giveHunger = false
   let hours = Date.now() / 1000 / 60 / 60
-  if (hours - feedTime.first < 24 && feedTime.number < 3) {
+  if (firstFeedWasWithin24Hours(hours, feedTime)) {
     ++feedTime.number
     feedTime.last = hours
+    feedTime.canFeed = (feedTime.number > 2) ? false : true
   }
-  if (hours - feedTime.first >= 24) {
-    // reset again
+  if (firstFeedWas24HoursAgo(hours, feedTime)) {
     feedTime.number = 1
     feedTime.canFeed = true
     feedTime.first = hours
     feedTime.last = hours
   }
-  feedTime.giveHunger = false
-  feedTime.canFeed = (feedTime.number > 2) ? false : true
   return feedTime
 }
 
@@ -73,24 +93,37 @@ async function updateHungryPets (db) {
       let user = await db.get(`user/${hungryPets[i].petData.prevOwner}`)
       hungryPets[i].petData.hunger = incrementHunger(hungryPets[i].petData.hunger)
       hungryPets[i].petData.happiness = decrementHappiness(hungryPets[i].petData.happiness)
-      if (hungryPets[i].petData.hunger >= 100) {
+      if (isStarving(hungryPets[i])) {
         hungryPets[i].petData.health = decrementHealth(hungryPets[i].petData.health)
-        if (hungryPets[i].petData.health <= 0) {
-          delete user.pets[hungryPets[i].name]
-          user.deadPets = {
-            name: hungryPets[i],
-            alertUser: true
-          }
-          await db.put(`user/${user.id}`, user)
+        if (shouldKillPet(hungryPets[i])) {
+          await killPet(db, user, hungryPets[i])
+          return
         }
       }
       user.pets[hungryPets[i].name] = hungryPets[i]
-      console.log('updated pet:', hungryPets[i])
+      // console.log('updated pet:', hungryPets[i])
       await db.put(`user/${user.id}`, user)
     }
   } catch (err) {
+    console.log(err)
     throw err
   }
+}
+
+function isStarving (pet) {
+  return (pet.petData.hunger === 100) ? true : false
+}
+
+function shouldKillPet (pet) {
+  return (pet.petData.health === 0) ? true : false
+}
+
+async function killPet (db, user, pet) {
+  delete user.pets[pet.name]
+  user.deadPets = user.deadPets || {}
+  user.deadPets = Object.assign(user.deadPets, { name: pet.name, alertUser: true })
+  console.log(user.deadPets)
+  await db.put(`user/${user.id}`, user)
 }
 
 function shouldFeedPet (pet) {
@@ -120,7 +153,7 @@ function getAllHungryPets (db) {
         let pets = Object.values(entry.value.pets)
         for (let i = 0; i < pets.length; i++) {
           if (shouldFeedPet(pets[i])) {
-            console.log(pets[i])
+            // console.log('should feed', pets[i])
             entry.value.pets[pets[i].name].petData.feeding = createFeedTimeObj()
             hungryPets.push(pets[i])
           }
@@ -135,16 +168,20 @@ function getAllHungryPets (db) {
   })
 }
 
+const HAPPINESS_DECR = 10
+const HUNGER_INCR = 25
+const HEALTH_DECR = 10
+
 function decrementHappiness (happiness) {
-  return (happiness < 10) ? 0 : happiness - 10
+  return (happiness < HAPPINESS_DECR) ? 0 : happiness - HAPPINESS_DECR
 }
 
 function incrementHunger (hunger) {
-  return (hunger > 75) ? 100 : hunger + 25
+  return (HUNGER_INCR + hunger > 100) ? 100 : hunger + HUNGER_INCR
 }
 
 function decrementHealth (health) {
-  return (health < 10) ? 0 : health - 10
+  return (health < HEALTH_DECR ) ? 0 : health - HEALTH_DECR
 }
 
 function createFeedTimeObj () {
