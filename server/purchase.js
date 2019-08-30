de938f937d353ff3ea0ae1d850e2e09996e79baf
cyntran <cyntran@uw.edu>
2019-08-29 18:02:46 -0700

    hungry pets on the market get their scores from their hunger when they were sold or, if they are a brand new pet, get a 25 hunger score

--- server/purchase.js
+++ server/purchase.js
@@ -40,7 +40,7 @@ function setUserItemProperties (updatedUserInfo, item, buyData, newQuan) {
         "unicode": item.unicode,
         "isAnimal": true,
         "petData": {
-          "hunger": 25,
+          "hunger": (!item.petData.hunger) ? 25 : item.petData.hunger,
           "generation": item.petData.generation,
           "happiness": item.petData.happiness,
           "intelligence": item.petData.intelligence,
